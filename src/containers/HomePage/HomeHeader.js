d55f1e5f1a18c8b3051ac8a9012fb84add27e070
DoNgocSon <ngocsonse2001@gmail.com>
2022-09-03 16:14:11 +0700

    upload chuyen doi ngon ngu voi Redux

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -13,50 +13,60 @@ import lgGoiPhauThuat from "../../assets/images/goiphauthuat.jpg";
 import lgSanPhamYTe from "../../assets/images/khamtainha.png";
 import lgSucKhoeDoanhNghiep from "../../assets/images/khamnhakhoa.png";
 import { Fragment } from "react";
-
+import { FormattedMessage } from 'react-intl'; //Component thay doi ngon ngu
+import { LANGUAGES } from "../../utils"
+import { changeLanguageApp } from "../../store/actions";
 
 class HomeHeader extends Component {
 
+    changeLanguage = (language) => {
+        //fire redux event: actions
+        this.props.changeLanguageAppRedux(language);
+    }
+
     render() {
+        //lấy từ redux
+        let language = this.props.language;
+
         return (
             <Fragment>
                 <div className="home-header-container">
                     <div className="home-header-content">
                         <div className="left-content">
-                            <i class="fas fa-bars"></i>
+                            <i className="fas fa-bars"></i>
                             <img className="header-logo " alt="" src={logo} />
                         </div>
                         <div className="center-content">
                             <div className="child-content">
-                                <div><b>Chuyên khoa</b></div>
-                                <div className="subs-title">Tìm bác sĩ theo chuyên khoa</div>
+                                <div><b><FormattedMessage id="home-header.speciality"></FormattedMessage></b></div>
+                                <div className="subs-title"><FormattedMessage id="home-header.search-doctor"></FormattedMessage></div>
                             </div>
                             <div className="child-content">
-                                <div><b>Cơ sở y tế</b></div>
-                                <div className="subs-title">Chọn bệnh viện phòng khám</div>
+                                <div><b><FormattedMessage id="home-header.health-facility"></FormattedMessage></b></div>
+                                <div className="subs-title"><FormattedMessage id="home-header.select-room"></FormattedMessage></div>
                             </div>
                             <div className="child-content">
-                                <div><b>Bác sĩ</b></div>
-                                <div className="subs-title">Chọn bác sĩ giỏi</div>
+                                <div><b><FormattedMessage id="home-header.doctor"></FormattedMessage></b></div>
+                                <div className="subs-title"><FormattedMessage id="home-header.select-doctor"></FormattedMessage></div>
                             </div>
                             <div className="child-content">
-                                <div><b>Gói khám</b></div>
-                                <div className="subs-title">Khám sức khỏe tổng quát</div>
+                                <div><b><FormattedMessage id="home-header.fee"></FormattedMessage></b></div>
+                                <div className="subs-title"><FormattedMessage id="home-header.check-health"></FormattedMessage></div>
                             </div>
                         </div>
                         <div className="right-content">
-                            <div className="support"><i class="fas fa-question-circle">Hỗ trợ</i></div>
-                            <div className="language-vi">VN</div>
-                            <div className="language-en">EN</div>
+                            <div className="support"><i className="fas fa-question-circle"><FormattedMessage id="home-header.support"></FormattedMessage></i></div>
+                            <div className={language === LANGUAGES.VI ? "language-vi active" : "language-vi"}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}> VN</span></div>
+                            <div className={language === LANGUAGES.EN ? "language-en active" : "language-en"}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                         </div>
                     </div>
                 </div>
                 <div className="home-header-banner">
                     <div className="content-up">
-                        <div className="title1">NỀN TẢNG Y TẾ</div>
-                        <div className="title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+                        <div className="title1"><FormattedMessage id="banner.title1"></FormattedMessage></div>
+                        <div className="title2"><FormattedMessage id="banner.title2"></FormattedMessage></div>
                         <div className="search">
-                            <i class="fas fa-search"></i>
+                            <i className="fas fa-search"></i>
                             <input type="text" placeholder="Tìm chuyên khoa..." />
                         </div>
                     </div>
@@ -64,45 +74,44 @@ class HomeHeader extends Component {
                         <div className="option">
                             <div className="option-child">
                                 <div className="icon-child"><img src={lgKhamChuyenKhoa} alt="" /></div>
-                                <div className="text-child">Khám chuyên khoa</div>
+                                <div className="text-child"><FormattedMessage id="banner.child1"></FormattedMessage></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><img src={lgKhamTuXa} alt="" /></div>
-                                <div className="text-child">Khám từ xa</div>
+                                <div className="text-child"><FormattedMessage id="banner.child2"></FormattedMessage></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><img src={lgKhamTongQuat} alt="" /></div>
-                                <div className="text-child">Khám tổng quát</div>
+                                <div className="text-child"><FormattedMessage id="banner.child3"></FormattedMessage></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><img src={lgXetNghiemYHoc} alt="" /></div>
-                                <div className="text-child">Xét nghiệm y học</div>
+                                <div className="text-child"><FormattedMessage id="banner.child4"></FormattedMessage></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><img src={lgSucKhoeTinhThan} alt="" /></div>
-                                <div className="text-child">Sức khỏe tinh thần</div>
+                                <div className="text-child"><FormattedMessage id="banner.child5"></FormattedMessage></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><img src={lgKhamNhaKhoa} alt="" /></div>
-                                <div className="text-child">Khám nha khoa</div>
+                                <div className="text-child"><FormattedMessage id="banner.child6"></FormattedMessage></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><img src={lgGoiPhauThuat} alt="" /></div>
-                                <div className="text-child">Gói phẫu thuật</div>
+                                <div className="text-child"><FormattedMessage id="banner.child7"></FormattedMessage></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><img src={lgSanPhamYTe} alt="" /></div>
-                                <div className="text-child">Sản phẩm y tế</div>
+                                <div className="text-child"><FormattedMessage id="banner.child8"></FormattedMessage></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><img src={lgSucKhoeDoanhNghiep} alt="" /></div>
-                                <div className="text-child">Sức khỏe doanh nghiệp</div>
+                                <div className="text-child"><FormattedMessage id="banner.child9"></FormattedMessage></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </Fragment>
-
         )
     }
 }
@@ -110,11 +119,14 @@ class HomeHeader extends Component {
 const mapStateToProps = (state) => {
     return {
         isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+    return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
--- src/store/actions/appActions.js
+++ src/store/actions/appActions.js
@@ -1,10 +1,17 @@
 import actionTypes from './actionTypes';
 
 export const appStartUpComplete = () => ({
+    //ko truyền data
     type: actionTypes.APP_START_UP_COMPLETE
 });
 
 export const setContentOfConfirmModal = (contentOfConfirmModal) => ({
+    // truyền data
     type: actionTypes.SET_CONTENT_OF_CONFIRM_MODAL,
     contentOfConfirmModal: contentOfConfirmModal
+});
+
+export const changeLanguageApp = (languageInput) => ({
+    type: actionTypes.CHANGE_LANGUAGE,
+    language: languageInput
 });
