dbaeba3df714605a620b8cdccb8ece9e5d7a4ccc
DoNgocSon <ngocsonse2001@gmail.com>
2022-11-08 15:53:27 +0700

    Hoàn Thiện Menu Phân Quyền Dành Cho Admin

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { FormattedMessage } from 'react-intl';
 import * as actions from "../../store/actions";
 import Navigator from "../../components/Navigator";
 import { adminMenu } from "./menuApp";
@@ -12,7 +12,7 @@ class Header extends Component {
     this.props.changeLanguageAppRedux(language)
   }
   render() {
-    const { processLogout, language } = this.props;
+    const { processLogout, language, userInfo } = this.props;
 
     return (
       <div className="header-container">
@@ -20,7 +20,12 @@ class Header extends Component {
         <div className="header-tabs-container">
           <Navigator menus={adminMenu} />
         </div>
+
         <div className="languages">
+          <span className="welcome">
+            <FormattedMessage id="home-header.welcome" />
+            {userInfo && userInfo.firstName ? userInfo.firstName : ''}
+          </span>
           <span className={language === LANGUAGES.VI ? "language-vi active" : "language-vi"}
             onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}>VN</span>
           <span className={language === LANGUAGES.EN ? "language-en active" : "language-en"}
@@ -38,6 +43,7 @@ class Header extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    userInfo: state.user.userInfo,
     language: state.app.language,
   };
 };
